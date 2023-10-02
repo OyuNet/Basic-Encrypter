dd9cbc5a84a7ff5c6cca6f1086a73081ad47d7dd
OyuNet <oyunarda662@gmail.com>
2023-10-02 23:44:51 +0300

    encrypt için switch case yapısından kurtuldum artık gerekli data sql'den çekiliyor direkt olarak.

--- db.js
+++ db.js
@@ -13,13 +13,18 @@ export const addSymbol = async (symbol, value) => {
     await db.set(`keys.${symbol}`, value)
 }
 
+export const checkSymbol = async (symbol) => {
+    const status = await db.has(`keys.${symbol}`);
+    return status;
+}
+
 export const getSymbol = async (symbol) => {
-    const status = db.has(`keys.${symbol}`);
+    const status = await db.has(`keys.${symbol}`);
     const value = status ? await db.get(`keys.${symbol}`) : null;
     return value;
 }
 
-export const removeSymbol = async (symbol) => {
+export const removeSymbol = async (symbol) => { // i dunno why i wrote this. but this feature is here and ready, dear ladies and gentlemans. i didnt even check this func status. so use wisely. thanks.
     symbol = symbol.split("").lenght > 1 ? null : symbol;
 
     if (!(await db.has(symbol))) {
--- encrypt.js
+++ encrypt.js
@@ -1,3 +1,5 @@
+import { checkSymbol, getSymbol } from "./db.js";
+
 export const encrypt = (data, key) => {
     const keyArray = key.split("");
     const dataArray = data.split("");
@@ -42,73 +44,11 @@ export const arrayToNumbers = (data) => {
     let NumberArray = [];
 
     data.map((x) => {
-        switch(x) {
-            case("a"):
-                NumberArray.push(1);
-            case("b"):
-                NumberArray.push(2);
-            case("c"):
-                NumberArray.push(3);
-            case("ç"):
-                NumberArray.push(4);
-            case("d"):
-                NumberArray.push(5);
-            case("e"):
-                NumberArray.push(6);
-            case("f"):
-                NumberArray.push(7);
-            case("g"):
-                NumberArray.push(8);
-            case("ğ"):
-                NumberArray.push(9);
-            case("h"):
-                NumberArray.push(10);
-            case("ı"):
-                NumberArray.push(11);
-            case("i"):
-                NumberArray.push(12);
-            case("j"):
-                NumberArray.push(13);
-            case("k"):
-                NumberArray.push(14);
-            case("l"):
-                NumberArray.push(15);
-            case("m"):
-                NumberArray.push(16);
-            case("n"):
-                NumberArray.push(17);
-            case("o"):
-                NumberArray.push(18);
-            case("ö"):
-                NumberArray.push(19);
-            case("p"):
-                NumberArray.push(20);
-            case("r"):
-                NumberArray.push(21);
-            case("s"):
-                NumberArray.push(22);
-            case("ş"):
-                NumberArray.push(23);
-            case("t"):
-                NumberArray.push(24);
-            case("u"):
-                NumberArray.push(25);
-            case("ü"):
-                NumberArray.push(26);
-            case("v"):
-                NumberArray.push(27);
-            case("y"):
-                NumberArray.push(28);
-            case("z"):
-                NumberArray.push(29);
-            case("w"):
-                NumberArray.push(30);
-            case("q"):
-                NumberArray.push(31);
-            case("x"):
-                NumberArray.push(32);
-            default:
-                NumberArray.push(0);
+        if (checkSymbol(x)) {
+            const number = getSymbol(x)
+            NumberArray.push(number)
+        } else {
+            NumberArray.push(0)
         }
     })
 
