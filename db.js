1b673c866c2babd4f0ec620e5c0c1a6394b88e37
OyuNet <oyunarda662@gmail.com>
2023-10-02 23:36:43 +0300

    biraz daha shitcode

--- db.js
+++ db.js
@@ -4,13 +4,19 @@ const db = new QuickDB();
 
 export const addSymbol = async (symbol, value) => {
     value = Number.isInteger(value) ? value : null;
-    symbol = symbol.split("").lenght() > 1 ? null : symbol;
+    symbol = symbol.length > 1 ? null : symbol;
 
     if (await (db.has(symbol))) {
         return console.error(`DB already has this symbol. Specified symbol: ${symbol}`)
     }
 
-    await db.add(`keys.${symbol}`, value)
+    await db.set(`keys.${symbol}`, value)
+}
+
+export const getSymbol = async (symbol) => {
+    const status = db.has(`keys.${symbol}`);
+    const value = status ? await db.get(`keys.${symbol}`) : null;
+    return value;
 }
 
 export const removeSymbol = async (symbol) => {
